24ca7736fc21caec5da7519d9756fa6e5b7c7177
Vaibhav Malik <vaibhavmalik2018@gmail.com>
2024-12-06 10:25:22 +0530

    Processing JSON data for better benchmarking

--- src/main/java/translateapi/LibreTranslate.java
+++ src/main/java/translateapi/LibreTranslate.java
@@ -5,6 +5,14 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.JsonObject;
+import space.dynomake.libretranslate.Language;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
 
 public class LibreTranslate {
     public static final String API_URL = "http://localhost:5000/translate";
@@ -15,6 +23,7 @@ public class LibreTranslate {
             payload.addProperty("q", text);
             payload.addProperty("source", sourceLang);
             payload.addProperty("target", targetLang);
+            // payload.addProperty("format", "text");
             payload.addProperty("format", "text");
             payload.addProperty("alternatives", 0);
 
@@ -31,21 +40,32 @@ public class LibreTranslate {
 
             // Print the response and latency
             System.out.println("Response: " + response.body());
+            System.out.println("Text length: " + text.length());
             System.out.println("Latency: " + (endTime - startTime) / 1_000_000 + " ms");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static void translateJSONData(String filePath, String sourceLang, String targetLang) {
+        try (FileReader reader = new FileReader(filePath)) {
+            JSONArray jsonArray = new JSONArray(new JSONTokener(reader));
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                String text = jsonArray.getString(i);
+
+                if (text.length() < 5000) {
+                    // Process each text here
+                    libreTranslate(text, sourceLang, targetLang);
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        libreTranslate(TextExamples.largeString, "auto", "hi");
-
-        for (String text: TextExamples.hindiTexts) {
-            libreTranslate(text, "auto", "en");
-        }
-
-        for (TextExamples.TextExample text: TextExamples.TextExample.values()) {
-            libreTranslate(text.getText(), "auto", "hi");
-        }
+        // translateJSONData("/Users/vaibhav.malik/Downloads/translateapi/src/main/resources/german-data.json", Language.GERMAN.getCode(), Language.ENGLISH.getCode());
+        translateJSONData("/Users/vaibhav.malik/Downloads/translateapi/src/main/resources/jeopardy-questions.json", Language.ENGLISH.getCode(), Language.HINDI.getCode());
     }
 }
